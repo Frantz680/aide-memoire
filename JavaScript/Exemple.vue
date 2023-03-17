23e24cb080f7fbbd10d961b2f571f220ee6ca4c5
François <68595779+Frantz680@users.noreply.github.com>
2023-03-17 13:31:52 +0100

    Update Exemple.vue

--- JavaScript/Exemple.vue
+++ JavaScript/Exemple.vue
@@ -1,19 +1,33 @@
 <template>
-    <div class="select">
-        <label for="annee-select">Années</label>
-        <br>
-        <!-- <select v-model="selected" @change="PostDeclaListeModif(annee)"> -->
-        <select class="form-select form-select-lg mb-3" v-model="selected" @change="GetDeclaListeModif($event)" >
-            <option disabled value="">Selectionner une date</option>
-            <option v-for="annee in UniqueAnnee" :value="annee" v-bind:key="annee">{{ annee }} </option>
+    <Navigation pageName="DeclarationsListe" />
+    <Spinner :loading="Loading" />
+    <div class="container main-content">
 
-        </select>
-    </div>
+        <header class="jumbotron">
+            <h3>Modèles déclarations</h3>
+        </header>
 
-    <span class="ombre">
+        <div class="select">
+            <label for="annee-select">Années</label>
+            <br>
+            <!-- <select v-model="selected" @change="PostDeclaListeModif(annee)"> -->
+            <select class="form-select form-select-lg mb-3" v-model="selected" @change="GetDeclaListeModif($event)" >
+                <option disabled value="">Selectionner une date</option>
+                <option v-for="annee in UniqueAnnee" :value="annee" v-bind:key="annee">{{ annee }} </option>
+
+            </select>
+        </div>
+
         <EasyDataTable :headers="TableUsersHeaders" :items="Declarations" :rows-per-page=100 :rows-items=[100] buttons-pagination
-            alternating table-class-name="customize-table" rowsPerPageMessage="Résultats par page" theme-color="#002f64a6"
-            rowsOfPageSeparatorMessage="sur" emptyMessage="Aucune donnée" class="table-success">
+            alternating table-class-name="customize-table" rowsPerPageMessage="Résultats par page" theme-color="#002f64a6" 
+            header-text-direction="center" body-text-direction="center"
+            rowsOfPageSeparatorMessage="sur" emptyMessage="Aucune donnée" class="table-success" style="overflow: hidden !important;">
+
+            <!-- Code -->
+            <template #item-code="item">
+                <span class="overme">{{ item.code }}</span>
+                <span v-if="item.code.length > 27" class="tooltip">{{ item.code }}</span>
+            </template>
 
             <!-- Details -->
             <template #item-details="item">
@@ -31,10 +45,15 @@
                         </span>
                         <span v-else>
                             <Popper hover arrow  content="Modifier à nouveau" placement="top">
-                                <font-awesome-icon v-on:click="actif(item.idModeleDeclarationLigne)" icon="edit" class="clickable" />
+                                <font-awesome-icon v-on:click="actif(item.idModeleDeclarationLigne)" icon="edit" class="clickable background-blue"/>
                             </Popper>
                         </span>
-                        {{ item.libellePerso }}
+                        <div class="overme">
+                            {{ item.libellePerso }}
+                        </div>
+                        <span  v-if="item.libellePerso.length > 27" class="tooltip">
+                            {{ item.libellePerso }}
+                        </span>
                     </div>
 
                     <input  v-else type="text" v-bind:value="item.libellePerso" v-bind:id="item.idModeleDeclarationLigne" v-on:keyup.enter="onEntrer(item.idModeleDeclarationLigne, 'libelle')">
@@ -46,11 +65,13 @@
             </template>
 
         </EasyDataTable>
-    </span>
 
+    </div>
 </template>
 
 <script>
+import Navigation from "@/components/Nav.vue";
+import Spinner from "@/components/Spinner.vue";
 import { getUrlWs } from "@/utilities";
 import authHeader from '@/services/auth-header';
 import { set } from "lodash";
@@ -103,6 +124,7 @@ export default {
     },
 
     components: {
+        Navigation, Spinner,
     },
     methods: {
         getListAnnee(){
@@ -116,6 +138,7 @@ export default {
                     // On recuperer les années uniques
                     this.UniqueAnnee = listAnnee.filter(this.isUnique);
 
+                    // On declare l'année a null
                     let anneeMax = null;
 
                     // On appelle la fonction
@@ -152,8 +175,6 @@ export default {
                     // On ajouter la valeur au select
                     this.selected = anneeMax;
 
-                    console.log(response.data)
-
                     // On ajout les déclartions
                     this.Declarations = response.data.listModeleDeclarationRequete;
 
@@ -184,24 +205,29 @@ export default {
         onEntrer(id, cible){
             this.Loading = true;
 
-            // On selection tous les inputs
-            let tableInput = document.querySelectorAll(".table-input");
-
             // On declare la variable valeur
             var valeur = 0;
 
-            // On boucle sur tous nos inputs
-            tableInput.forEach((item) => {
+            if (cible == 'bloc') {
+                // On selection tous les inputs
+                let tableInput = document.querySelectorAll(".table-input");
 
-                // Si un input est egale avec notre id
-                if(item.id == id){
+                // On boucle sur tous nos inputs
+                tableInput.forEach((item) => {
 
-                    // On lui donne la valeur de l'item
-                    valeur = item.value;
-                    
-                }
-            })
+                    // Si un input est egale avec notre id
+                    if(item.id == id){
 
+                        // On lui donne la valeur de l'item
+                        valeur = item.value;
+
+                    }
+                })
+            }
+            else {
+                // On recupere la valeur du libelle
+                valeur = document.getElementById(id).value;
+            }
             
 
             // Param
@@ -217,6 +243,7 @@ export default {
 
                 console.log(response.data);
 
+                // On appelle la fonction avec en parametre l'année
                 this.GetDeclaListe(this.anneeFilter);
             
             }).catch((err) => {
@@ -257,8 +284,16 @@ export default {
 @import "@/scss/variables";
 @import "@/scss/popper";
 
+.container {
+    height: auto;
+}
+
 .background-red {
-    color: rgb(243, 100, 100) !important;
+    color: rgb(255, 60, 60) !important;
+}
+
+.background-blue {
+    color: rgba(0, 47, 100);;
 }
 
 .custom-tooltip {
@@ -276,6 +311,10 @@ export default {
     -moz-backdrop-filter: blur(5px);
     transition: all 0.5s ease-out;
     text-align: center;
+    table-layout: fixed; 
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
 }
 
 .table-input:hover {
@@ -288,13 +327,17 @@ export default {
     box-shadow:  4px 4px 60px 8px rgba(0, 0, 0, .2);
 }
 
+
 .customize-table {
+    --easy-table-row-border: 1px solid #445269;
+
     --easy-table-header-background-color: rgba(0, 47, 100, 0.651);
     --easy-table-header-font-size: 20px;
     --easy-table-header-font-color: #ffffff;
 
 
-    --easy-table-body-row-height: 50px;
+    --easy-table-body-row-height: 100px;
+    --easy-table-body-row-width: 100px;
     --easy-table-body-row-font-size: 14px;
 
     --easy-table-footer-background-color: rgba(0, 47, 100, .65);
@@ -304,17 +347,12 @@ export default {
     --easy-table-footer-height: 50px;
 
     --easy-table-body-row-hover-font-color: #2d3a4f;
-  --easy-table-body-row-hover-background-color: #eee;
+    --easy-table-body-row-hover-background-color: #eee;
 
     --easy-table-scrollbar-track-color: #2d3a4f;
     --easy-table-scrollbar-color: #2d3a4f;
     --easy-table-scrollbar-thumb-color: #4c5d7a;;
     --easy-table-scrollbar-corner-color: #2d3a4f;
-
-}
-
-.vue3-easy-data-table__main {
-    overflow: none;
 }
 
 .text-align-right {
@@ -325,7 +363,41 @@ export default {
     padding: 7px;
     width: 200px;
     border: solid 2px rgba(0, 47, 100, 0.651);
-    box-shadow: 20px 20px 40px -6px rgb(0 0 0 / 20%);
+    box-shadow: 10px 2px 10px -6px rgb(0 0 0 / 20%);
+}
+
+.overme {
+    display:inline-block;
+    width:200px;
+    white-space: nowrap;
+    overflow:hidden !important;
+    text-overflow: ellipsis;
+    vertical-align: bottom;
+    padding-left: 5px;
+}
+
+.overme:hover:after {
+   cursor: pointer;
+}
+
+.tooltip {
+    width: 100%;
+    bottom: 55%;
+    visibility: hidden;
+    background-color: #fff;
+    padding: 20px;
+    -webkit-box-shadow: 0 0 50px 0 rgba(0,0,0,0.3);
+    opacity: 0;
+    transition: opacity 0.8s ease; 
+    transition: all 0.5s ease-out;
+    border-radius: 5px;
+}
+
+.overme:hover + .tooltip {
+    visibility: visible;
+    transition: opacity 0.8s ease;
+    transition: all 0.5s ease-out;
+    opacity: 1;
 }
 
 </style>
